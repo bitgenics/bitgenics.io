daa1d6dcd2f3e601bd3aa2aff9ae63d82b9ffbe7
Erwin van der Koogh <erwin@bitgenics.io>
2017-11-24 14:41:29 +1100

    Add <head> info

---
+++ src/linc.server.config.js
@@ -0,0 +1,28 @@
+const config = {
+  getStaticHead: (req) => {
+    return `
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#65457b">
+    <!--link rel="manifest" href="%PUBLIC_URL%/manifest.json">
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" -->
+    <meta name="description" property="og:description" content="Linc is a front-end delivery platform for single page web applications designed to improve search engine indexing and social sharing" >
+    
+    <meta name="author" content="Bitgenics">
+    <meta name="robots" content="index, follow">
+
+    <meta name="twitter:card" content="summary" >
+    <meta name="twitter:site" content="@bitgenics" >
+    <meta name="twitter:title" content="Bitgenics - Linc a front-end delivery platform for single page web applications" >
+
+    <meta property="og:title" content="Bitgenics - Linc a front-end delivery platform for single page web applications" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.bitgenics.io" />
+    <meta property="og:image" content="https://www.bitgenics.io/logo.png" />
+
+    <title>Bitgenics - Linc a front-end delivery platform for single page web applications</title>
+    `
+  }
+};
+
+export default config
