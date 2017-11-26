8249511cec6c47ba73ab2c4b97bcbb646235f4e3
Alastair Taft <ali.c.taft@gmail.com>
2017-11-27 00:41:41 +1100

    Removed fonts.css and index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './fonts.css'
 import { Route } from 'react-router'
 import HomePage from './HomePage'
 import StartupPage from './StartupPage'
@@ -8,6 +7,30 @@ import PrivacyPolicyPage from './PrivacyPolicyPage'
 import LegalPage from './LegalPage'
 import BlogPage from './BlogPage'
 import ArticlePage from './ArticlePage'
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  body {
+    margin: 0;
+    font-size: 1em;
+    line-height: 1.85714286em;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #666;
+    font-weight: 400;
+    font-family: 'Lato','Verdana','Helvetica', sans-serif;
+    background-color: #fafafa;
+  }
+
+  h1, h2, h3, h4, h5 {
+    font-family: 'Rubik','Helvetica', sans-serif;
+    color: #252525;
+  }
+
+  p {
+    font-size: 14px;
+  }
+`
 
 class App extends Component {
   render() {
