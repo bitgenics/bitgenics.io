391c7b54cc881c967703b12c38b345dd6c9a7341
Sander Huijsen <sander.huijsen@gmail.com>
2018-01-17 10:04:30 +0800

    Made some experimental changes around intercom.

--- pages/_document.js
+++ pages/_document.js
@@ -41,17 +41,15 @@ export default class MyDocument extends Document {
           <meta property="og:image" content="https://www.bitgenics.io/static/full_logo.svg" />
 
           <title>Bitgenics - Linc a front-end delivery platform for single page web applications</title>
-          <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/lv0ic9po';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+          <script dangerouslySetInnerHTML={{__html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/lv0ic9po';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()`}}>
+          </script>
         </Head>
         <body>
           <div className='root'>
             <Main />
           </div>
           <NextScript />
-          window.Intercom("boot", {
-            app_id: "lv0ic9po"
-          });
-          window.Intercom("update");
+          <script dangerouslySetInnerHTML={{__html: `window.Intercom("boot",{app_id:"lv0ic9po"});window.Intercom("update");`}}/>
         </body>
       </html>
     )
