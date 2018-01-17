1d4670b12129d56d782a1fa3cd009e1346bc1e3d
Catherine Jones <cath@bitgirl101.com>
2018-01-17 12:46:13 +1100

    add intercom code snippests

--- pages/_document.js
+++ pages/_document.js
@@ -27,26 +27,31 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="theme-color" content="#65457b" />
           <meta name="description" property="og:description" content="Linc is a front-end delivery platform for single page web applications designed to improve search engine indexing and social sharing" />
-    
+
           <meta name="author" content="Bitgenics" />
           <meta name="robots" content="index, follow" />
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@bitgenics" />
           <meta name="twitter:title" content="Bitgenics - Linc a front-end delivery platform for single page web applications" />
-          
+
           <meta property="og:title" content="Bitgenics - Linc a front-end delivery platform for single page web applications" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.bitgenics.io" />
           <meta property="og:image" content="https://www.bitgenics.io/static/full_logo.svg" />
 
           <title>Bitgenics - Linc a front-end delivery platform for single page web applications</title>
+          <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/lv0ic9po';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
         </Head>
         <body>
           <div className='root'>
             <Main />
           </div>
           <NextScript />
+          window.Intercom("boot", {
+            app_id: "lv0ic9po"
+          });
+          window.Intercom("update");
         </body>
       </html>
     )
